15bbfd5f49017dea8a01a92b54fba6d0347b665a
¨Alper <alpertas.cpp@gmail.com>
2022-09-01 00:59:18 +0300

    add skills component (still developing)

--- src/component/Flatlist.js
+++ src/component/Flatlist.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import { Text , FlatList } from 'react-native';
+import {Text, FlatList} from 'react-native';
 
 const PFlatList = props => {
-    const {
-        style,
-        data,
-        renderItem,
-        horizontal,
-    } = props;
-    
-    return (
-        <FlatList 
-            keyExtractor={item => item.id}
-            renderItem={renderItem}
-            data={data}
-            {...horizontal === true ? horizontal : null}
-            horizontal
-            contentContainerStyle={{
-                flexDirection: 'column',
-                flexWrap: 'wrap'
-            }}
-        />
-    );
+  const {style, data, renderItem, horizontal} = props;
+
+  return (
+    <FlatList
+      keyExtractor={item => item.id}
+      renderItem={renderItem}
+      data={data}
+      numColumns={3} // set number of columns
+      //columnWrapperStyle={style.row}  // space them out evenly
+      //{...(horizontal === true ? horizontal : null)}
+      vertical
+      contentContainerStyle={{
+        flexDirection: 'column',
+      }}
+    />
+  );
 };
 
 export default PFlatList;
